60d778192f52628efa39fc3da8e78c0b6a475d89
Franco Carrizo <francarri_6@hotmail.com>
2024-04-30 07:02:20 +0000

    initial commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,13 +19,45 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+
+app.get("/api/:date", function (req, res) {
+  console.log('req',req.params)
+  const date = req.params.date;
+  const dateNow =  Date.now(date)
+  //const unix =  getUtcDate(date).getTime();
+
+  const utcInitial = new Date(Date.UTC(0, 0, 0, 0, 0, 0));
+  const utcGenerate =  getUtcDate(date)
+  const unix = parseInt((utcGenerate / 1000).toFixed(0))
+  const unix2 = parseInt(utcGenerate)
+  console.log('sape',date ,'.,,-',
+  unix,'-----',unix,'---',unix2,'-----',utcGenerate,'.-----',utcInitial
+  )
+  res.json({
+    unix,
+    utc:utcGenerate.toUTCString()
+  });
 });
 
+const getUtcDate= (date)=> {
+  const newDate = new Date(date)
+  const year = newDate.getFullYear() 
+  const month = newDate.getMonth()
+  const day = newDate.getDay()
+  const hour = newDate.getHours()
+  const minute = newDate.getMinutes()
+  const second = newDate.getSeconds()
+  const dateUtc = new Date(`${day} ${month} ${year} ${hour}:${minute}:${second} PDT`);
+   
+console.log(`UT ${dateUtc.getTime()}`)
+  return dateUtc;
+}
 
-
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
